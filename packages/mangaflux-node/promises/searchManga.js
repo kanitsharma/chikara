49d298fb5f143637341a941a4dbcaac4a3def091
kanitsharma <kanitshrm09@gmail.com>
2018-08-07 20:59:34 +0530

    updated search

--- packages/mangaflux-node/promises/searchManga.js
+++ packages/mangaflux-node/promises/searchManga.js
@@ -1,17 +1,21 @@
 const searchManga = keys =>
-  new Promise(resolve => {
-    const cursor = global.db.collection("detailedList").find(
-      { title_kw: keys },
-      {
-        title: 1,
-        description: 1,
-        categories: 1,
-        image: 1,
-        author: 1,
-        status: 1
-      }
-    );
-    resolve(cursor.next());
-  });
+  new Promise(resolve =>
+    resolve(
+      global.db
+        .collection("detailedList")
+        .find(
+          { title_kw: keys },
+          {
+            title: 1,
+            description: 1,
+            categories: 1,
+            image: 1,
+            author: 1,
+            status: 1
+          }
+        )
+        .next()
+    )
+  );
 
 module.exports = { searchManga };
